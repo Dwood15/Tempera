79f4ecc64912edaf7cc10b26f104198a31c621ad
Dwood <dwood15@users.noreply.github.com>
2018-04-25 18:04:14 -0600

    Adding some player stuff

--- main.cpp
+++ main.cpp
@@ -4,20 +4,20 @@
 	Copyright � 2009 SilentK, Abyll
 	Copyright � 2018 Dwood
 
-	This file is part of haloforge.
+	This file is part of Tempera.
 
-    haloforge is free software: you can redistribute it and/or modify
+    Tempera is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    haloforge is distributed in the hope that it will be useful,
+    Tempera is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with haloforge.  If not, see <http://www.gnu.org/licenses/>.
+    along with Tempera.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
